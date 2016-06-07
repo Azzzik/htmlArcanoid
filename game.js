6622fcd32058e799a8d14fadd39c86c8e29e5466
Azzzik <polskoy.st@gmail.com>
2016-06-08 01:11:41 +0300

    Paddle was added
    I add paddle and restrict it movements by canvas borders.

--- game.js
+++ game.js
@@ -1,13 +1,22 @@
+// canvas vars
 var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-	
+var ctx = canvas.getContext("2d");	
 var x = canvas.width/2;
 var y = canvas.height-30;
+//ball vars
 var speed = 0.5;
 var dx = speed;
 var dy = -speed;
 var ballRadius = 10;
+//paddle vars
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
+
+//random color generator
 function rCol()
 {
 col = Math.round(255.0*Math.random());
@@ -19,6 +28,8 @@ b=col.toString(16);
 col="#"+r+g+b;
 return col;
 }
+
+//default ball color
 ctx.fillStyle = "#0095DD"
 
 function drawBall() {
@@ -34,23 +45,57 @@ function drawBall() {
     ctx.fillStyle = rancol;} 	
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     x += dx;
     y += dy;
 	if(x  > canvas.width -ballRadius|| x + dx < ballRadius) {
-    dx = -dx;
-	
-	
-	
+    dx = -dx;	
 }
-
     if(y  > canvas.height -ballRadius|| y + dy < ballRadius) {
     dy = -dy;
 	
 	
 }
+drawPaddle();
+
+if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
 }
+else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
+}
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
 
 setInterval(draw, 10);
